81450f51dca2e460933c97a80a99e5c1706682c1
Marc Fornos <marc.fornos@gmail.com>
2023-10-10 12:27:46 +0200

    extract server listen and fix registrations

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ import FastifySwaggerUI from '@fastify/swagger-ui';
 import FastifyHealthcheck from 'fastify-healthcheck';
 
 import version from './version.js';
+import { logger } from './environment.js';
 import { ServerOptions } from './types.js';
 import {
   Root,
@@ -17,57 +18,30 @@ import {
   Matching
 } from './services/index.js';
 import { NotFound } from './errors.js';
-
-const environment = process.env.NODE_ENV || 'development';
-
-const envToLogger: Record<string, any> = {
-  development: {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hostname',
-      },
-    },
-  },
-  production: true,
-  test: false,
-};
+import { ZodError } from 'zod';
 
 /**
  * Creates and starts the server process with specified options.
  *
  * @param {ServerOptions} opts - Options for configuring the server.
  */
-export function createServer(
+export async function createServer(
   opts: ServerOptions
 ) {
   const server = Fastify({
-    logger: envToLogger[environment]
+    logger
   });
 
-  server.register(FastifySwagger, {
-    openapi: {
-      info: {
-        title: 'XCM Monitoring Service',
-        version
-      }
+  server.setErrorHandler(function (error, _, reply) {
+    if (error instanceof NotFound) {
+      reply.status(404).send(error.message);
+    } else if (error instanceof ZodError) {
+      reply.status(400).send(error.message);
+    } else {
+      // to parent handler
+      reply.send(error);
     }
   });
-
-  server.register(FastifySwaggerUI, {
-    routePrefix: '/documentation'
-  });
-
-  server.register(FastifyHealthcheck, {
-    exposeUptime: true
-  });
-
-  server.register(Root);
-  server.register(Storage, opts);
-  server.register(Configuration, opts);
-  server.register(Matching);
-  server.register(Monitoring);
 
   const closeListeners = closeWithGrace({
     delay: opts.grace
@@ -87,22 +61,28 @@ export function createServer(
     done();
   });
 
-  server.setErrorHandler(function (error, _, reply) {
-    if (error instanceof NotFound) {
-      reply.status(404).send(error.message);
-    } else {
-      // to parent handler
-      reply.send(error);
+  await server.register(FastifySwagger, {
+    openapi: {
+      info: {
+        title: 'XCM Monitoring Service',
+        version
+      }
     }
   });
 
-  server.listen({
-    port: opts.port,
-    host: opts.host
-  }, function (err, _) {
-    if (err) {
-      server.log.error(err);
-      process.exit(1);
-    }
+  await server.register(FastifySwaggerUI, {
+    routePrefix: '/documentation'
   });
+
+  await server.register(FastifyHealthcheck, {
+    exposeUptime: true
+  });
+
+  await server.register(Root);
+  await server.register(Storage, opts);
+  await server.register(Configuration, opts);
+  await server.register(Matching);
+  await server.register(Monitoring);
+
+  return server;
 }
