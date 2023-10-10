3718a154630cf618eeaeb99feb6ca5e07c52db9b
Marc Fornos <marc.fornos@gmail.com>
2023-10-10 12:26:51 +0200

    extract environment conf

---
+++ src/environment.ts
@@ -0,0 +1,17 @@
+export const environment = process.env.NODE_ENV || 'development';
+
+const envToLogger: Record<string, any> = {
+  development: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+  production: true,
+  test: false,
+};
+
+export const logger = envToLogger[environment];
