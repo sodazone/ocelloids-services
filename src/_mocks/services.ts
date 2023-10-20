4ae187de3630b65202c87b09c14fa53f1a283cd3
Marc Fornos <marc.fornos@gmail.com>
2023-10-20 12:00:18 +0200

    add mock data

--- src/_mocks/services.ts
+++ src/_mocks/services.ts
@@ -6,43 +6,7 @@ import { Janitor } from '../services/storage/janitor.js';
 import { $ServiceConfiguration } from '../services/configuration.js';
 import Connector from '../services/networking/connector.js';
 import { DB } from '../services/types';
-
-export const _configToml = `
-[[networks]]
-id = 0
-name = "local_relay"
-
-  [networks.provider]
-  type = "rpc"
-  url = "ws://localhost:9000"
-
-[[networks]]
-id = 1_000
-name = "local_1"
-relay = "local_reay"
-
-  [networks.provider]
-  type = "rpc"
-  url = "ws://localhost:9001"
-
-[[networks]]
-id = 2_000
-name = "local_2000"
-relay = "local_reay"
-
-  [networks.provider]
-  type = "rpc"
-  url = "ws://localhost:9002"
-
-[[networks]]
-id = 3_000
-name = "local_3000"
-relay = "local_reay"
-
-  [networks.provider]
-  type = "rpc"
-  url = "ws://localhost:9003"
-`;
+import { _configToml } from './data.js';
 
 export const _Pino = pino({
   enabled: false
