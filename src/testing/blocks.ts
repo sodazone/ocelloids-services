3a71f856aeb03da3d98fe01890e056906c740369
Marc Fornos <marc.fornos@gmail.com>
2023-10-25 14:31:23 +0200

    rename test/ to testing/

---
+++ src/testing/blocks.ts
@@ -0,0 +1,55 @@
+import * as path from 'node:path';
+import * as url from 'url';
+import { readFileSync } from 'node:fs';
+
+import { decode } from 'cbor-x';
+import { TypeRegistry, Metadata } from '@polkadot/types';
+import { createSignedBlockExtended } from '@polkadot/api-derive';
+import type { SignedBlock, EventRecord, AccountId } from '@polkadot/types/interfaces';
+
+import { HexString } from '../services/monitoring/types.js';
+
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+export type BinBlock = {
+  block: Uint8Array;
+  events: Uint8Array[];
+  author?: Uint8Array;
+}
+
+type RpcResult = {
+  jsonrpc: string,
+  id: number,
+  result: HexString
+}
+
+export function testBlocksFrom(file: string, metadataFile: string) {
+  const bufferBlock = readFileSync(path.resolve(__dirname, '__data__', file));
+  const blocks: BinBlock[] = decode(bufferBlock);
+
+  const m = readFileSync(path.resolve(__dirname, '__data__/metadata', metadataFile)).toString();
+  const r : RpcResult = JSON.parse(m);
+
+  const registry = new TypeRegistry() as any;
+  const metadata = new Metadata(registry, r.result);
+
+  registry.setMetadata(metadata);
+
+  return blocks.map(b => {
+    const block = registry.createType('SignedBlock', b.block);
+    const records = registry.createType('Vec<EventRecord>', b.events, true);
+    const author = registry.createType('AccountId', b.author);
+
+    return createSignedBlockExtended(
+      registry,
+      block as SignedBlock,
+      records as unknown as EventRecord[],
+      null,
+      author as AccountId
+    );
+  });
+}
+
+export const polkadotBlocks = testBlocksFrom('dmp-out.cbor.bin', 'polkadot.json');
+export const interlayBlocks = testBlocksFrom('hrmp-in-2032-success.cbor.bin', 'interlay.json');
+export const assetHubBlocks = testBlocksFrom('hrmp-out-1000.cbor.bin', 'asset-hub.json');
---
+++ src/testing/services.ts
@@ -0,0 +1,101 @@
+import { pino } from 'pino';
+import toml from 'toml';
+import { of } from 'rxjs';
+import { MemoryLevel } from 'memory-level';
+import { ApiRx, ApiPromise } from '@polkadot/api';
+
+import { SubsStore } from '../services/persistence/subs.js';
+import { Janitor } from '../services/persistence/janitor.js';
+import { $ServiceConfiguration } from '../services/configuration.js';
+import Connector from '../services/networking/connector.js';
+import { _configToml } from './data.js';
+import { Scheduler } from '../services/persistence/scheduler.js';
+
+export const _log = pino({
+  enabled: false
+});
+
+export const _config = $ServiceConfiguration.parse(
+  toml.parse(_configToml)
+);
+
+export const _connector = {
+  connect: () => ({
+    promise: {
+      '0': {
+        derive: {
+          chain: {
+            getBlock: () => {
+
+            }
+          }
+        }
+      } as unknown as ApiPromise,
+      '1000': {
+        derive: {
+          chain: {
+            getBlock: () => {
+            }
+          }
+        },
+        at: () => {
+          return Promise.resolve({
+            query: { }
+          });
+        }
+      } as unknown as ApiPromise,
+      '2000': {
+        derive: {
+          chain: {
+            getBlock: () => {
+            }
+          }
+        }
+      } as unknown as ApiPromise
+    },
+    rx: {
+      '0': of({
+        rpc: {
+          chain: {
+            subscribeFinalizedHeads: () => of({})
+          },
+        }
+      } as unknown as ApiRx),
+      '1000': of({
+        rpc: {
+          chain: {
+            subscribeFinalizedHeads: () => of({})
+          },
+        }
+      }),
+      '2000': of({
+        rpc: {
+          chain: {
+            subscribeFinalizedHeads: () => of({})
+          },
+        }
+      } as unknown as ApiRx)
+    }
+  })
+} as unknown as Connector;
+
+export const _rootDB = new MemoryLevel();
+export const _subsDB = new SubsStore(
+  _log, _rootDB, _config
+);
+
+export const _services = {
+  log: _log,
+  config: _config,
+  connector: _connector,
+  storage: {
+    root: _rootDB,
+    subs: _subsDB
+  },
+  scheduler: {
+    on: () => {}
+  } as unknown as Scheduler,
+  janitor: {
+    schedule: () => {}
+  } as unknown as Janitor
+};
