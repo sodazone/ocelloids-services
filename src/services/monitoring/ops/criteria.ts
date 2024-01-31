a5e1043994848ae3c2fbfd6463e45f68a294e52a
Xueying Wang <wang.xy.326@gmail.com>
2024-01-31 17:05:29 +0100

    expand sendersCriteria to filter in extraSigners #8

--- src/services/monitoring/ops/criteria.ts
+++ src/services/monitoring/ops/criteria.ts
@@ -3,12 +3,13 @@ import { Criteria } from '@sodazone/ocelloids';
 export function sendersCriteria(senders: string[] | '*') : Criteria {
   if (Array.isArray(senders)) {
     return {
-      'extrinsic.signer.id': { $in: senders }
+      $or: [
+        { 'extrinsic.signer.id': { $in: senders } },
+        { 'extrinsic.extraSigners.address.id': { $in: senders } }
+      ]
     };
   } else {
-    return {
-      'extrinsic.signer': { $exists: true }
-    };
+    return {};
   }
 }
 
