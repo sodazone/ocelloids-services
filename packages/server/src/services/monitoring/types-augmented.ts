ccb17c84331ddfe363c8598683d74d5cdc4c3ba2
Marc Fornos <marc.fornos@gmail.com>
2024-02-19 16:42:20 +0100

    extract client library types

---
+++ packages/server/src/services/monitoring/types-augmented.ts
@@ -0,0 +1,10 @@
+import { Observable } from 'rxjs';
+
+import type { Vec, Bytes } from '@polkadot/types';
+import type { PolkadotCorePrimitivesOutboundHrmpMessage } from '@polkadot/types/lookup';
+
+export type GetOutboundHrmpMessages = (hash: `0x${string}`)
+=> Observable<Vec<PolkadotCorePrimitivesOutboundHrmpMessage>>
+
+export type GetOutboundUmpMessages = (hash: `0x${string}`)
+=> Observable<Vec<Bytes>>
