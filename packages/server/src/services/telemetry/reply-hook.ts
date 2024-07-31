841a5f8958cff5d433a19ce267997f78329a136e
Marc Fornos <marc.fornos@gmail.com>
2024-07-31 20:15:05 +0200

    use routeOptions.url

--- packages/server/src/services/telemetry/reply-hook.ts
+++ packages/server/src/services/telemetry/reply-hook.ts
@@ -21,7 +21,7 @@ export function createReplyHook() {
     if (NO_ROUTE_STATUS.includes(reply.statusCode)) {
       reqHist.labels(statusCode.toString(), request.method, '-').observe(elapsedTime)
     } else {
-      reqHist.labels(statusCode.toString(), request.method, request.routerPath ?? '-').observe(elapsedTime)
+      reqHist.labels(statusCode.toString(), request.method, request.routeOptions.url ?? '-').observe(elapsedTime)
     }
   }
 }
