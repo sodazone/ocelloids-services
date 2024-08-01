4fbff33ec9428376988b9a3dd02ad8904c847cdb
Marc Fornos <marc.fornos@gmail.com>
2024-08-01 15:42:23 +0200

    fix wrong hex padding

--- packages/server/src/services/agents/steward/agent.ts
+++ packages/server/src/services/agents/steward/agent.ts
@@ -301,7 +301,7 @@ export class DataSteward implements Agent, Queryable {
             const asset: AssetMetadata = {
               id,
               // id native(aaee) index(u8)
-              xid: `0xaaee${i.toString(16).padEnd(2, '0')}`,
+              xid: `0xaaee${i.toString(16).padStart(2, '0')}`,
               updated: Date.now(),
               symbol,
               decimals: decimals[i],
