5308d95760c7bc4f7eaf6899fc5dec1d5a7dfda1
Marc Fornos <marc.fornos@gmail.com>
2024-10-24 09:41:51 +0200

    add download test tool

---
+++ packages/server/src/testing/tools/download.ts
@@ -0,0 +1,66 @@
+import fs from 'node:fs'
+import * as path from 'node:path'
+import * as url from 'url'
+
+import { Command, Option } from 'commander'
+import { pino } from 'pino'
+
+import { encodeBlock } from '@/services/ingress/watcher/codec.js'
+import { createArchiveClient } from '@/services/networking/index.js'
+
+const __dirname = url.fileURLToPath(new URL('..', import.meta.url))
+
+const networks = {
+  polkadot: 'wss://rpc.ibp.network/polkadot',
+  assethub: 'wss://polkadot-asset-hub-rpc.polkadot.io',
+  hydra: 'wss://hydradx.paras.ibp.network',
+} as Record<string, string>
+
+async function download([name, ws, height]: [string, string, string]) {
+  const logger = pino()
+  const client = await createArchiveClient(logger, name, ws)
+
+  logger.info('Downloading %s@%s', name, height)
+
+  const hash = await client.getBlockHash(height)
+
+  console.log(hash)
+
+  logger.info('Block hash %s', hash)
+
+  const block = await client.getBlock({
+    hash,
+    number: Number(height),
+  })
+
+  const dest = path.resolve(__dirname, `__data__/blocks/${client.chainId}`, `${block.number}.cbor`)
+
+  logger.info('Write %s', dest)
+
+  fs.writeFileSync(dest, encodeBlock(block))
+
+  client.disconnect()
+}
+
+const program = new Command()
+program
+  .name('download')
+  .description('Download blocks by height')
+  .argument('<height>', 'block height')
+  .addOption(
+    new Option('-n, --network <name>', 'network name').default('polkadot').choices(Object.keys(networks)),
+  )
+  .action(async (height, { network }) => {
+    if (!/^[0-9]+$/.test(height)) {
+      throw new Error('Height should be positive int')
+    }
+
+    const ws = networks[network]
+    if (ws) {
+      await download([network, ws, height])
+    } else {
+      throw new Error(`Network name not found: ${network}`)
+    }
+  })
+  .version('0.0.1')
+  .parse()
