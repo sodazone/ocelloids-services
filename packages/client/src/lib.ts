9d56a3a823c020fe29a37fa6d9723202405a4653
Xueying Wang <wang.xy.326@gmail.com>
2024-12-10 14:41:23 +0100

    fix bun exports

--- packages/client/src/lib.ts
+++ packages/client/src/lib.ts
@@ -5,7 +5,25 @@
  *
  * @packageDocumentation
  */
-export * from './server-types'
+export * from './client'
+export * from './types'
+export type {
+  SubscriptionId,
+  AnyJson,
+  HexString,
+  Message,
+  SignerData,
+  AgentId,
+  QueryParams,
+  QueryResult,
+  QueryPagination,
+  AnyQueryArgs,
+  NetworkURN,
+  AnyQueryResultItem,
+  NetworkInfo,
+  sourceSteward,
+  sourceXcm,
+} from './server-types'
 
 // The "export * as ___" syntax is not supported yet; as a workaround,
 // use "import * as ___" with a separate "export { ___ }" declaration
