bf5c4155d566b3831ef922b68e254fe1f65f13a4
Xueying Wang <wang.xy.326@gmail.com>
2025-01-09 13:51:04 +0100

    use stable archive rpc methods

--- packages/server/src/services/networking/client/archive.ts
+++ packages/server/src/services/networking/client/archive.ts
@@ -8,7 +8,6 @@ import {
   SystemEvent,
   getObservableClient,
 } from '@polkadot-api/observable-client'
-import { blockHeader } from '@polkadot-api/substrate-bindings'
 import { ChainSpecData, createClient } from '@polkadot-api/substrate-client'
 import {
   fixDescendantValues,
@@ -142,6 +141,22 @@ export class ArchiveClient extends EventEmitter implements ApiClient {
 
   async getBlock(hash: string): Promise<Block> {
     try {
+      const [block, events] = await Promise.all([this.#getBlock(hash), this.#getEvents(hash)])
+      return asSerializable({
+        hash,
+        number: BigInt(block.header.number).toString(),
+        parent: block.header.parentHash,
+        extrinsics: block.extrinsics.map((tx) => this.ctx.decodeExtrinsic(tx)),
+        events,
+      }) as Block
+    } catch (error) {
+      throw new Error(`[client:${this.chainId}] Failed to fetch block ${hash}.`, { cause: error })
+    }
+  }
+
+  /*
+  async getBlock(hash: string): Promise<Block> {
+    try {
       const [header, txs, events] = await Promise.all([
         this.getHeader(hash),
         this.#getBody(hash),
@@ -158,8 +173,9 @@ export class ArchiveClient extends EventEmitter implements ApiClient {
     } catch (error) {
       throw new Error(`[client:${this.chainId}] Failed to fetch block ${hash}.`, { cause: error })
     }
-  }
+  }*/
 
+  /*
   async getBlockHash(blockNumber: string | number | bigint): Promise<string> {
     try {
       const result = await this.#request<string[], [height: number]>('archive_unstable_hashByHeight', [
@@ -172,8 +188,39 @@ export class ArchiveClient extends EventEmitter implements ApiClient {
     } catch (error) {
       throw new Error(`[client:${this.chainId}] Failed to fetch block hash.`, { cause: error })
     }
+  }*/
+
+  async getBlockHash(blockNumber: string | number | bigint): Promise<string> {
+    try {
+      const result = await this.#request<string, [height: string]>('chain_getBlockHash', [
+        '0x' + Number(blockNumber).toString(16),
+      ])
+      if (result === null) {
+        throw new Error('Block hash not found')
+      }
+      return result
+    } catch (error) {
+      throw new Error(`[client:${this.chainId}] Failed to fetch block hash.`, { cause: error })
+    }
   }
 
+  async getHeader(hash: string): Promise<BlockInfo> {
+    try {
+      const header = await this.#request<{ parentHash: string; number: string }, [hash: string]>(
+        'chain_getHeader',
+        [hash],
+      )
+      return {
+        parent: header.parentHash,
+        hash,
+        number: Number(BigInt(header.number)),
+      }
+    } catch (error) {
+      throw new Error(`[client:${this.chainId}] Failed to fetch header for hash ${hash}.`, { cause: error })
+    }
+  }
+
+  /*
   async getHeader(hash: string): Promise<BlockInfo> {
     try {
       const encodedHeader = await this.#request<string, [hash: string]>('archive_unstable_header', [hash])
@@ -186,7 +233,7 @@ export class ArchiveClient extends EventEmitter implements ApiClient {
     } catch (error) {
       throw new Error(`[client:${this.chainId}] Failed to fetch header for hash ${hash}.`, { cause: error })
     }
-  }
+  }*/
 
   async getStorageKeys(
     keyPrefix: string,
@@ -251,6 +298,34 @@ export class ArchiveClient extends EventEmitter implements ApiClient {
     }
   }
 
+  async #getBlock(hash: string) {
+    try {
+      const result = await this.#request<
+        {
+          block: {
+            header: {
+              parentHash: string
+              number: string
+              stateRoot: string
+              extrinsicsRoot: string
+              digest?: {
+                logs: any[]
+              }
+            }
+            extrinsics: string[]
+          }
+        },
+        [hash: string]
+      >('chain_getBlock', [hash])
+      return result?.block
+    } catch (error) {
+      throw new Error(`[client:${this.chainId}] Failed to fetch block body for hash ${hash}.`, {
+        cause: error,
+      })
+    }
+  }
+
+  /*
   async #getBody(hash: string) {
     try {
       const result = await this.#request<[tx: string], [hash: string]>('archive_unstable_body', [hash])
@@ -260,7 +335,7 @@ export class ArchiveClient extends EventEmitter implements ApiClient {
         cause: error,
       })
     }
-  }
+  }*/
 
   async #getEvents(hash: string) {
     try {
