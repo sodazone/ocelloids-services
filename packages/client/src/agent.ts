84faaeef1f35c447d2cde38f077efd6385645b7e
Marc Fornos <marc.fornos@gmail.com>
2025-01-23 13:19:17 +0100

    document agent factory functions

--- packages/client/src/agent.ts
+++ packages/client/src/agent.ts
@@ -12,22 +12,61 @@ import { XcmInputs, XcmMessagePayload } from './xcm/types'
 
 type KnownAgentIds = 'xcm' | 'steward' | 'informant'
 
+/**
+ * Creates an agent instance.
+ *
+ * @template I The type of subscription inputs.
+ * @param id The ID of the agent to create.
+ * @param opts Configuration options for the OcelloidsClient.
+ * @returns An instance of the specified agent.
+ */
 function createAgent<I = AnySubscriptionInputs>(id: KnownAgentIds, opts: OcelloidsClientConfig) {
   return new OcelloidsClient(opts).agent<I>(id)
 }
 
+/**
+ * Creates an 'xcm' agent instance.
+ *
+ * @example
+ *
+ * ```typescript
+ * import { createXcmAgent } from "@sodazone/ocelloids-client";
+ *
+ * const agent = createXcmAgent({
+ *   httpUrl: "http://127.0.0.1:3000",
+ *   wsUrl: "ws://127.0.0.1:3000"
+ * });
+ *
+ * const ws = await agent.subscribe(...);
+ * ```
+ *
+ * @param opts Configuration options for the OcelloidsClient.
+ * @returns An object with methods for subscribing to XCM events and general client API methods.
+ */
 export function createXcmAgent(
   opts: OcelloidsClientConfig,
 ): SubscribableApi<XcmInputs, XcmMessagePayload> & OcelloidsClientApi {
   return createAgent<XcmInputs>('xcm', opts)
 }
 
+/**
+ * Creates a 'steward' agent instance.
+ *
+ * @param opts Configuration options for the OcelloidsClient.
+ * @returns An object with methods for querying asset metadata and general client API methods.
+ */
 export function createStewardAgent(
   opts: OcelloidsClientConfig,
 ): QueryableApi<StewardQueryArgs, AssetMetadata> & OcelloidsClientApi {
   return createAgent('steward', opts)
 }
 
+/**
+ * Creates an 'informant' agent instance.
+ *
+ * @param opts Configuration options for the OcelloidsClient.
+ * @returns An object with methods for subscribing to events and general client API methods.
+ */
 export function createInformantAgent(opts: OcelloidsClientConfig): SubscribableApi & OcelloidsClientApi {
   return createAgent('informant', opts)
 }
