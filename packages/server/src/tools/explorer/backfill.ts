96ab8a8e645f707cf89c1133bfa9e41c3b05273c
Xueying Wang <wang.xy.326@gmail.com>
2025-06-23 20:17:07 +0200

    add backfill tool

---
+++ packages/server/src/tools/explorer/backfill.ts
@@ -0,0 +1,172 @@
+import { asJSON } from '@/common/util.js'
+import { HexString } from '@/lib.js'
+import { createXcmDatabase } from '@/services/agents/xcm/explorer/repositories/db.js'
+import { XcmRepository } from '@/services/agents/xcm/explorer/repositories/journeys.js'
+import { matchEvent } from '@/services/agents/xcm/ops/util.js'
+import { GenericXcmInboundWithContext } from '@/services/agents/xcm/types/messages.js'
+import { createSubstrateClient } from '@/services/networking/substrate/client.js'
+import { Block, extractEvents } from '@/services/networking/substrate/index.js'
+import { resolveDataPath } from '@/services/persistence/util.js'
+import { pino } from 'pino'
+import { Observable, concatMap, filter, from, map, takeWhile, timer } from 'rxjs'
+
+const log = pino()
+const moon = await createSubstrateClient(log, 'urn:ocn:polkadot:2004', 'wss://moonbeam.ibp.network')
+
+function backfillBlocks$({ start, end }: { start: number; end: number }): Observable<Block> {
+  return timer(0, 100).pipe(
+    map((index) => start + index),
+    takeWhile((blockNumber) => blockNumber <= end),
+    concatMap((blockNumber) =>
+      from(moon.getBlockHash(blockNumber)).pipe(concatMap((blockHash) => from(moon.getBlock(blockHash)))),
+    ),
+  )
+}
+
+function getMessageId(instructions: any) {
+  for (const instruction of instructions.value) {
+    if (instruction.type === 'SetTopic') {
+      return typeof instruction.value === 'string' ? instruction.value : instruction.value.asHex()
+    }
+  }
+}
+
+const [, , dbPathArg, startArg, endArg] = process.argv
+const dbPath = dbPathArg
+const startBlock = startArg ? Number(startArg) : -1
+const endBlock = endArg ? Number(endArg) : -1
+
+if (dbPath === undefined || startBlock < 0 || endBlock <= startBlock) {
+  console.log('usage: backfill.js /db/path 12000000 12000100')
+  process.exit(1)
+}
+
+const filename = resolveDataPath('lilp.xcm-explorer.sqlite', dbPath)
+log.info('[xcm:explorer] database at %s', filename)
+
+const { db, migrator: _migrator } = createXcmDatabase(filename)
+const migrator = _migrator
+const repository = new XcmRepository(db)
+await migrator.migrateToLatest()
+
+const { nodes } = await repository.listFullJourneys(
+  {
+    destinations: ['urn:ocn:polkadot:2004'],
+    status: ['timeout'],
+  },
+  {
+    limit: 100,
+  },
+)
+const mappedNodes = nodes.map((journey) => ({
+  id: journey.id,
+  correlationId: journey.correlation_id,
+  messageId: getMessageId(journey.instructions),
+}))
+
+console.log('MMMM', mappedNodes.length, mappedNodes)
+
+const METHODS_XCMP_QUEUE = ['Success', 'Fail']
+
+backfillBlocks$({ start: startBlock, end: endBlock })
+  .pipe(
+    extractEvents(),
+    map((event) => {
+      if (matchEvent(event, 'XcmpQueue', METHODS_XCMP_QUEUE)) {
+        const xcmpQueueData = event.value
+
+        return new GenericXcmInboundWithContext({
+          event: event,
+          extrinsicHash: event.extrinsic?.hash as HexString,
+          blockHash: event.blockHash as HexString,
+          blockNumber: event.blockNumber,
+          timestamp: event.timestamp,
+          extrinsicPosition: event.extrinsicPosition,
+          messageHash: xcmpQueueData.message_hash,
+          messageId: xcmpQueueData.message_id,
+          outcome: event.name === 'Success' ? 'Success' : 'Fail',
+          error: xcmpQueueData.error,
+        })
+      } else if (matchEvent(event, 'MessageQueue', 'Processed')) {
+        const { id, success, error } = event.value
+        // Received event only emits field `message_id`,
+        // which is actually the message hash in chains that do not yet support Topic ID.
+        const messageId = id
+        const messageHash = messageId
+
+        return new GenericXcmInboundWithContext({
+          event: event,
+          extrinsicHash: event.extrinsic?.hash as HexString,
+          blockHash: event.blockHash as HexString,
+          blockNumber: event.blockNumber,
+          timestamp: event.timestamp,
+          messageHash,
+          messageId,
+          outcome: success ? 'Success' : 'Fail',
+          error,
+        })
+      } else if (matchEvent(event, 'DmpQueue', 'ExecutedDownward')) {
+        const { message_id, outcome } = event.value
+
+        // Received event only emits field `message_id`,
+        // which is actually the message hash in chains that do not yet support Topic ID.
+        const messageId = message_id
+        const messageHash = messageId
+
+        return new GenericXcmInboundWithContext({
+          event: event,
+          extrinsicHash: event.extrinsic?.hash as HexString,
+          blockHash: event.blockHash as HexString,
+          blockNumber: event.blockNumber,
+          timestamp: event.timestamp,
+          messageHash,
+          messageId,
+          outcome: outcome.type === 'Complete' ? 'Success' : 'Fail',
+          error: null,
+        })
+      }
+
+      return null
+    }),
+    filter((msg) => msg !== null),
+  )
+  .subscribe({
+    next: async (msg) => {
+      console.log('RECEIVED', msg)
+      try {
+        const found = mappedNodes.find((n) => n.messageId === msg.messageId)
+        if (found) {
+          const journey = await repository.getJourneyByCorrelationId(found.correlationId)
+          if (!journey) {
+            throw new Error(`Journey not found for correlation id ${found.correlationId}`)
+          }
+          journey.stops[journey.stops.length - 1].to = {
+            chainId: 'urn:ocn:polkadot:2004',
+            blockHash: msg.blockHash,
+            blockNumber: msg.blockNumber,
+            timestamp: msg.timestamp,
+            status: msg.outcome,
+            extrinsic: {
+              blockPosition: msg.extrinsicPosition,
+              hash: msg.extrinsicHash,
+            },
+            event: {
+              blockPosition: (msg.event as any)?.blockPosition,
+              module: (msg.event as any)?.module,
+              name: (msg.event as any)?.name,
+            },
+          }
+          await repository.updateJourney(found.id, {
+            status: msg.outcome === 'Success' ? 'received' : 'failed',
+            recv_at: msg.timestamp,
+            stops: asJSON(journey.stops),
+          })
+        }
+      } catch (error) {
+        log.error(error)
+      }
+    },
+    complete: () => {
+      console.log('STREAM COMPLETE')
+    },
+  })
