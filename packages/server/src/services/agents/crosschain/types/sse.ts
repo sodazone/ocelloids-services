f6469dbb0eefabaf3da3e046c8f0a66d9b981e9f
Xueying Wang <wang.xy.326@gmail.com>
2025-10-10 17:42:46 +0200

    fix crosschain sse filters

--- packages/server/src/services/agents/crosschain/types/sse.ts
+++ packages/server/src/services/agents/crosschain/types/sse.ts
@@ -1,13 +1,21 @@
 import { z } from 'zod'
 
-import { $RawJourneyFilters } from './queries.js'
+import { $NetworkString } from '@/common/types.js'
+import { $BaseJourneyFilters, $JourneyProtocols, $JourneyStatus } from './queries.js'
 
 /**
  * @public
  */
-export const $XcServerSentEventArgs = $RawJourneyFilters
+export const $XcServerSentEventArgs = $BaseJourneyFilters
   .extend({
-    id: z.string().optional(),
+    assets: z.optional(z.union([z.string(), z.array(z.string()).min(1).max(50)])),
+    origins: z.optional(z.union([$NetworkString, z.array($NetworkString).min(1).max(50)])),
+    destinations: z.optional(z.union([$NetworkString, z.array($NetworkString).min(1).max(50)])),
+    networks: z.optional(z.union([$NetworkString, z.array($NetworkString).min(1).max(50)])),
+    protocols: z.optional(z.union([$JourneyProtocols, z.array($JourneyProtocols).min(1).max(2)])),
+    status: z.optional(z.union([$JourneyStatus, z.array($JourneyStatus).min(1).max(4)])),
+    actions: z.optional(z.union([z.string(), z.array(z.string()).min(1).max(5)])),
+    id: z.optional(z.string()),
   })
   .refine(
     (data) => {
