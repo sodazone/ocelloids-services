d3024d3ec55bd11b397cca7516123e58f096c44f
Xueying Wang <wang.xy.326@gmail.com>
2025-11-12 19:08:51 +0100

    fix evm ingress consumer

--- packages/server/src/services/networking/evm/ingress/local.ts
+++ packages/server/src/services/networking/evm/ingress/local.ts
@@ -6,6 +6,7 @@ import { Services } from '@/services/types.js'
 import { BlockWithLogs } from '../types.js'
 import { EvmIngressConsumer } from './types.js'
 import { EvmWatcher } from './watcher.js'
+import { HexString } from '@/lib.js'
 
 export class EvmLocalConsumer
   extends LocalIngressConsumer<EvmWatcher, BlockWithLogs, Chain>
@@ -13,5 +14,9 @@ export class EvmLocalConsumer
 {
   constructor(ctx: Services) {
     super(ctx, new EvmWatcher(ctx))
+  }
+
+  async getTransactionReceipt(chainId: string, txHash: HexString) {
+    return await this.watcher.getTransactionReceipt(chainId, txHash)
   }
 }
