30bc5594707aa64c811e733e62015bde7082ec0c
Xueying Wang <wang.xy.326@gmail.com>
2026-01-15 19:58:21 +0100

    add MRL sUSDS

--- packages/server/src/services/agents/steward/metadata/overrides.ts
+++ packages/server/src/services/agents/steward/metadata/overrides.ts
@@ -436,6 +436,16 @@ export const assetOverrides: Omit<AssetMetadata, 'updated'>[] = [
     externalIds: [],
   },
   {
+    chainId: 'urn:ocn:polkadot:2004',
+    id: '0xda430218862d3db25de9f61458645dde49a9e9c1',
+    xid: '0xda430218862d3db25de9f61458645dde49a9e9c1',
+    name: 'Savings USDS',
+    symbol: 'sUSDS',
+    decimals: 18,
+    raw: {},
+    externalIds: [],
+  },
+  {
     chainId: 'urn:ocn:solana:101',
     id: 'so11111111111111111111111111111111111111112',
     xid: '0x069b8857feab8184fb687f634618c035dac439dc1aeb3b5598a0f00000000001',
