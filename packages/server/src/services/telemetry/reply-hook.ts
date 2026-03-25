d7406838ac4535d81a60848d98e016f14632dd81
Marc Fornos <marc.fornos@gmail.com>
2026-03-25 19:06:38 +0100

    Convert Response Time Histogram to seconds

--- packages/server/src/services/telemetry/reply-hook.ts
+++ packages/server/src/services/telemetry/reply-hook.ts
@@ -6,8 +6,8 @@ const NO_ROUTE_STATUS = [401, 404]
 
 export function createReplyHook() {
   const reqHist = new Histogram({
-    name: 'oc_fastify_response_time_ms',
-    help: 'HTTP response time in milliseconds.',
+    name: 'oc_fastify_response_time_seconds',
+    help: 'HTTP response time in seconds.',
     labelNames: ['status', 'method', 'route'],
   })
   return async (request: FastifyRequest, reply: FastifyReply) => {
@@ -17,13 +17,11 @@ export function createReplyHook() {
 
     const { statusCode, elapsedTime } = reply
 
+    const durationSeconds = elapsedTime / 1000
+
     // only add route labels for non 401 or 404 status
-    if (NO_ROUTE_STATUS.includes(reply.statusCode)) {
-      reqHist.labels(statusCode.toString(), request.method, '-').observe(elapsedTime)
-    } else {
-      reqHist
-        .labels(statusCode.toString(), request.method, request.routeOptions.url ?? '-')
-        .observe(elapsedTime)
-    }
+    const route = NO_ROUTE_STATUS.includes(statusCode) ? '-' : (request.routeOptions.url ?? '-')
+
+    reqHist.labels(statusCode.toString(), request.method, route).observe(durationSeconds)
   }
 }
