6ae9e855bd3590d2ac621e0c853c9195fa0810d9
Marc Fornos <marc.fornos@gmail.com>
2026-03-27 12:53:34 +0100

    Add prefix columns and indexes to xc_journeys

--- packages/server/src/services/agents/crosschain/repositories/schema/pg.ts
+++ packages/server/src/services/agents/crosschain/repositories/schema/pg.ts
@@ -43,6 +43,8 @@ export async function up(db: Kysely<any>): Promise<void> {
       .addColumn('to', 'varchar(255)', (cb) => cb.notNull())
       .addColumn('from_formatted', 'varchar(255)')
       .addColumn('to_formatted', 'varchar(255)')
+      .addColumn('from_prefix', 'varchar(42)')
+      .addColumn('to_prefix', 'varchar(42)')
       .addColumn('sent_at', 'bigint')
       .addColumn('recv_at', 'bigint')
       .addColumn('created_at', 'bigint', (cb) => cb.notNull())
@@ -178,6 +180,34 @@ export async function up(db: Kysely<any>): Promise<void> {
       .ifNotExists()
       .on('xc_journeys')
       .column('trip_id')
+      .execute()
+
+    await db.schema
+      .createIndex('xc_journeys_from_prefix_sent_at_id_index')
+      .ifNotExists()
+      .on('xc_journeys')
+      .columns(['from_prefix', 'sent_at', 'id'])
+      .execute()
+
+    await db.schema
+      .createIndex('xc_journeys_to_prefix_sent_at_id_index')
+      .ifNotExists()
+      .on('xc_journeys')
+      .columns(['to_prefix', 'sent_at', 'id'])
+      .execute()
+
+    await db.schema
+      .createIndex('xc_journeys_from_sent_at_id_index')
+      .ifNotExists()
+      .on('xc_journeys')
+      .columns(['from', 'sent_at', 'id'])
+      .execute()
+
+    await db.schema
+      .createIndex('xc_journeys_to_sent_at_id_index')
+      .ifNotExists()
+      .on('xc_journeys')
+      .columns(['to', 'sent_at', 'id'])
       .execute()
 
     await db.schema
