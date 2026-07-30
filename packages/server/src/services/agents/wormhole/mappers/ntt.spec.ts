04393e849f78fb7b7852dffee85b2e0d331e94ef
Marc Fornos <marc.fornos@gmail.com>
2026-07-30 12:05:12 +0200

    Reformat NTT spec asset array

--- packages/server/src/services/agents/wormhole/mappers/ntt.spec.ts
+++ packages/server/src/services/agents/wormhole/mappers/ntt.spec.ts
@@ -9,13 +9,15 @@ const cases: TestCase[] = [
       type: 'transfer',
       from: '0x7f72d8e4d53a9acc52e40c01d504c9ed7099b512',
       to: '0x7f72d8e4d53a9acc52e40c01d504c9ed7099b512',
-      assets: [{
-        asset: 'urn:ocn:ethereum:1|0xd166337499e176bbc38a1fbd113ab144e5bd2df7',
-        symbol: '???',
-        amount: '370006138853',
-        decimals: 8,
-        usd: 3297.04700202,
-      }],
+      assets: [
+        {
+          asset: 'urn:ocn:ethereum:1|0xd166337499e176bbc38a1fbd113ab144e5bd2df7',
+          symbol: '???',
+          amount: '370006138853',
+          decimals: 8,
+          usd: 3297.04700202,
+        },
+      ],
     },
   },
 ]
